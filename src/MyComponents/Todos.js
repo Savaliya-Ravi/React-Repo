cd5634a45f29e249a22ac21cb98a61ac574a23ae
Savaliya-Ravi <98387226+Savaliya-Ravi@users.noreply.github.com>
2023-07-26 23:30:45 +0530

    Update Todos.js

--- src/MyComponents/Todos.js
+++ src/MyComponents/Todos.js
@@ -1,95 +1,109 @@
-// import React from 'react'
-// import {TodoItem} from "./TodoItem";
-// const Todos = (props) => {
-//     let myStyle = {
-//         minHeight: "70vh",
-//         margin: "40px auto"
-//     }
-//     return (
-//         <div className="container" style={myStyle}>
-//             <h3 className="my-3">Todos List</h3>
-//             {props.todos.length===0? "No Todos to display":  
-//             props.todos.map((todo)=>{
-//                 // console.log(todo.sno);
-//                 return (<TodoItem todo={todo} key={todo.sno} onDelete={props.onDelete} onEdit={props.onEdit}/>   
-//                 )
-//             })
-//               } 
-//         </div>
-//     )
-// }
-
 // export default Todos;
-
-
-
-import React, { useState } from 'react';
+import React, { useState} from 'react';
 import { TodoItem } from './TodoItem';
 
-const Todos = ({ todos, onDelete }) => {
+const Todos = ({ todos,setTodos, onDelete }) => {
   // State to keep track of the TodoItem being edited
-  const [editedTodo, setEditedTodo] = useState(todos);
+  const [editedTodo, setEditedTodo] = useState(null);
+  const [title, setTitle] = useState('');
+  const [desc, setDesc] = useState('');
+
 
   // Function to handle editing
   const handleEdit = (todo) => {
     setEditedTodo(todo);
+    setTitle(todo.title);
+    setDesc(todo.desc);
+    // Scroll to the top of the page when "Edit" button is clicked
+    window.scrollTo({ top: 0, behavior: 'smooth' });
   };
 
-  // Function to handle form submission when editing
-  const handleEditSubmit = (e) => {
+  // Function to handle form submission when adding or editing
+  const handleSubmit = (e) => { 
     e.preventDefault();
-    // Update the TodoItem in the Todos list
-    // First, remove the editedTodo from the list
-    const updatedTodos = todos.filter((todo) => todo !== editedTodo);
-    // Then add the modified editedTodo back to the list
-    updatedTodos.push(editedTodo);
-    setEditedTodo(null); // Reset the editedTodo state
-    setTodos(updatedTodos); // Update the Todos list
+    if (!title || !desc) {
+      alert('Title or Description cannot be blank');
+    } else {
+      if (editedTodo) {
+        // Update the TodoItem in the Todos list
+        const updatedTodos = todos.map( (todo) => (todo === editedTodo ? { title, desc } : todo));
+        setTodos(updatedTodos);
+        setEditedTodo(null);
+      } else {
+        // Add the new TodoItem to the Todos list
+        const sno = todos.length === 0 ? 0 : todos[todos.length - 1].sno + 1;
+        const newTodo = { sno, title, desc };
+        setTodos([...todos, newTodo]);
+      }
+    //   // Clear the form fields after submission
+    //   setTitle('');
+    //   setDesc('');
+    }
+  };
+
+  // Function to handle canceling the edit
+  const handleCancelEdit = () => {
+    setEditedTodo(null);
+    setTitle('');
+    setDesc('');
   };
 
   // ... Rest of your code ...
 
   return (
     <>
-      {/* Display the TodoItems and pass the handleEdit function */}
-      {todos.map((todo) => (
-        <TodoItem key={todo.sno} todo={todo} onDelete={onDelete} onEdit={handleEdit} />
-      ))}
+       <div className="container">
 
-      {/* Display the form for editing */}
-      {editedTodo && (
-        <form onSubmit={handleEditSubmit}>
-          <div className="mb-3">
-            <label htmlFor="editTitle" className="form-label">
-              Edit Todo Title
-            </label>
-            <input
-              type="text"
-              value={editedTodo.title}
-              onChange={(e) => setEditedTodo({ ...editedTodo, title: e.target.value })}
-              className="form-control"
-              id="editTitle"
-            />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="editDesc" className="form-label">
-              Edit Todo Description
-            </label>
-            <input
-              type="text"
-              value={editedTodo.desc}
-              onChange={(e) => setEditedTodo({ ...editedTodo, desc: e.target.value })}
-              className="form-control"
-              id="editDesc"
-            />
-          </div>
-          <button type="submit" className="btn btn-sm btn-success">
-            Save Changes
+   
+
+      {/* Display the form for adding or editing */}
+      <form onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label htmlFor="title" className="form-label">
+          <h5>{editedTodo ? 'Edit Todo Title' : 'Todo Title'}</h5>
+          </label>
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            className="form-control"
+            id="title"
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="desc" className="form-label">
+            <h5>{editedTodo ? 'Edit Todo Description' : 'Todo Description'}</h5>
+          </label>
+          <input
+            type="text"
+            value={desc}
+            onChange={(e) => setDesc(e.target.value)}
+            className="form-control"
+            id="desc"
+          />
+        </div>
+        {editedTodo ? (
+          <>
+            <button type="submit" className="btn btn-sm my-3 btn-success">
+              Save Changes
+            </button>
+            <button type="button" className="btn btn-sm my-3 btn-secondary mx-2" onClick={handleCancelEdit}>
+              Cancel
+            </button>
+          </>
+        ) : (
+          <button type="submit" className="btn btn-sm my-3 btn-success">
+            Add Todo
           </button>
-        </form>
-      )}
+        )}
+      </form>
 
       {/* ... Rest of your code ... */}
+       {/* Display the TodoItems and pass the handleEdit function */}
+      {todos.map((todo) => (
+        <TodoItem todo={todo} onDelete={onDelete} onEdit={handleEdit} />
+      ))}
+      </div> 
     </>
   );
 };
